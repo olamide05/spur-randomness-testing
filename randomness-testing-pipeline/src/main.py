55337493867b2811fffcaebf9b66733b6fae217f
Mahmoud Adetoro Alimi <132193157+olamide05@users.noreply.github.com>
2026-06-12 18:57:31 +0100

    Implement NIST frequency test

--- randomness-testing-pipeline/src/main.py
+++ randomness-testing-pipeline/src/main.py
@@ -1,11 +1,15 @@
 from parsers.loader import load_bitstream, clean_bitstream, validate_bitstream, load_and_process_bitstream
 from stats.basic_stats import stats
+from tests.frequency_test import frequency_test
 def main():
     filepath = 'sample_bitstream.txt'
     try:
         bitstream = load_and_process_bitstream(filepath)
         statistics = stats(bitstream)
+        frequency_result = frequency_test(bitstream)
         print(statistics)
+        print(frequency_result)
+        
     except Exception as e:
         print(f"Error processing bitstream: {e}")
 
